bedbb0bdc19bc57a251d6db1289fcb08dea64127
elen7c9 <enasiyati@gmail.com>
2018-03-01 18:48:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var h, q, d, n, p;
+var rest = 0;
+
+if(currency>=50){
+  h = Math.round(currency/50);
+  rest = currency%50;
+
+  if(rest>=25){
+    q = Math.round(rest/25);
+    rest=currency%25;
+  }
+  else q=0;
+
+  if(rest>=10){
+    d=Math.round(rest/10);
+    rest=currency%10;
+  }
+  else d=0;
+
+  if(rest>=5){
+    n=Math.round(rest/5);
+    rest=currency%5;
+    p=rest;
+  }
+  else n=0;
+
+}
+else h=0;
+
+
+var arr = [];
+if(h!==0) arr.push('"H":'+h);
+if(q!==0) arr.push('"Q":'+q);
+if(d!==0) arr.push('"D":'+d);
+if(n!==0) arr.push('"N":'+n);
+if(p!==0) arr.push('"P":'+p);
+    
+    return arr;
 }
